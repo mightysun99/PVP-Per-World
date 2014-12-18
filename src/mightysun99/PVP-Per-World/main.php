d470b8bff395d0b5f812250d0cb1b3cf98816261
mightysun99 <mightysun99@users.noreply.github.com>
2014-12-18 12:50:09 +1000

    Update main.php
    Needs tidying

--- src/mightysun99/PVP-Per-World/main.php
+++ src/mightysun99/PVP-Per-World/main.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace mightysun99\PVP-Per-World;
+
+use pocketmine\event\entity\EntityDamageEvent;
+
+public function onHurt(EntityDamageEvent $event) {
+      $entity = $event->getEntity();
+      $level = $event->getLevel()->getLevelName();
+     if($entity instanceof Player) {
+        if($level == "world"){
+          //pvp only for world with name "world"
+        }else{
+        //for other worlds
+        $event->setCancelled();
+       }
+     }
+}
