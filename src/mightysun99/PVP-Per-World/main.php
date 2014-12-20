6eac9c50c8918e974663fc6f088dce37dc867e76
Darunia18 <darunia1818@gmail.com>
2014-12-19 20:47:11 -0500

    Update main.php
    Added the easier config file option and the config checker to see if they edited it.

--- src/mightysun99/PVP-Per-World/main.php
+++ src/mightysun99/PVP-Per-World/main.php
@@ -3,25 +3,32 @@
 namespace mightysun99\PVP-Per-World;
 
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener{
       private $WorldOn;
-      private $WorldOff;
       
 public function onEnable() {
-    	@mkdir($this->getDataFolder());
-      $this->configFile = (new Config($this->getDataFolder()."config.yml", Config::YAML, array()))->getAll();
+    	$config = $this->getConfig();
+    	$this->WorldOn = $config->get("WorldOn");
+    	if($this->WorldOn == "false"){ //This checks to see if the config has been edited or not
+    	      $this->getLogger()->info("Please set your PVP-enabled world in the config.");
+    	}
+}
+
+public function onDisable(){
+      $this->getLogger()->info("PVP-Per-World has been disabled.");
 }
 
 public function onHurt(EntityDamageEvent $event) {
       $entity = $event->getEntity();
       $level = $event->getLevel()->getLevelName();
-     if($entity instanceof Player) {
-        if($level == "world"){
-          //pvp only for world with name "world"
-        }else{
-        //for other worlds
-        $event->setCancelled();
-       }
-     }
+      if($entity instanceof Player) {
+            if($level == "world"){
+            //pvp only for world with name "world"
+            }else{
+                  //for other worlds
+                  $event->setCancelled();
+            }
+      }
 }
